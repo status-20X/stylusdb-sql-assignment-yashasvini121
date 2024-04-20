01e6ddc0b965e61109beb751e4b84d6865f67143
yashasvini <ysmarina1234@gmail.com>
2024-04-20 23:54:19 +0530

    updating test:2

--- tests/step-02/index.test.js
+++ tests/step-02/index.test.js
@@ -1,9 +1,14 @@
 const readCSV = require('../../src/csvReader');
 
 test('Read CSV File', async () => {
-    const data = await readCSV('./sample.csv');
-    expect(data.length).toBeGreaterThan(0);
-    expect(data.length).toBe(3);
-    expect(data[0].name).toBe('John');
-    expect(data[0].age).toBe('30'); //ignore the string type here, we will fix this later
+    const data = await readCSV('sample-data/sample.csv');
+    // checking if data is of type Array
+    if (!Array.isArray(data)) {
+        throw new Error('Output Data of readCSV is not of Array Type.');
+    }
+
+    // checking if data is not empty
+    if (data.length === 0) {
+        throw new Error('Output Data of readCSV is empty.');
+    }
 });
